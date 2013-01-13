16d6cc0897746f468b7c18dc7cd5f7f4a520e656
Martin <martin.runelov@gmail.com>
2013-01-13 18:29:43 +0100

    added a database file and a prime view

--- links/lemonparty/models.py
+++ links/lemonparty/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=300)
+    personal_number = models.IntegerField()
+
+    def __unicode__(self):
+        return "[" + self.name + "," + str(self.personal_number) + "]"
--- links/urls.py
+++ links/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^lemonparty/$', 'lemonparty.views.index'),
-    url(r'^lemonparty/time$', 'lemonparty.views.current_datetime'),
+    url(r'^lemonparty/template/(?P<low_prime>\d+)/(?P<high_prime>\d+)/$', 'lemonparty.views.template'),
+    url(r'^lemonparty/primes/(?P<prime_ceiling>\d+)/$', 'lemonparty.views.primes'),
     url(r'^admin/', include(admin.site.urls)),
 )
