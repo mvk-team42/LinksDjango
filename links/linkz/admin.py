e9ceafeac38108cb74aedf5ac5808804658dfc48
johntu <john.brynte@gmail.com>
2013-01-27 19:46:36 +0100

    Fortsatte leka lite med admin-interfacet enligt djangobook.

--- links/linkz/admin.py
+++ links/linkz/admin.py
@@ -1,5 +1,10 @@
 from models import Link, Daniel
 from django.contrib import admin
 
-admin.site.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('description', 'href', 'ratingz')
+    search_fields =('description','href',)
+    ordering = ('description',)
+
+admin.site.register(Link, LinkAdmin)
 admin.site.register(Daniel)
